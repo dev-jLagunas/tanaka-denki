a66c486b2825d04850093b8b0f38534e4c8491b9
Juan Lagunas <dev.jlagunas@gmail.com>
2025-11-11 08:03:23 +0900

    feat: set up html, styles, and props for base ashieve chard

--- app/components/AccomplishmentInfo.vue
+++ app/components/AccomplishmentInfo.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div>
+  <section>
     <BaseSectionHeader
       bgColor="bg-brand-blue"
       title="施工実績"
@@ -18,5 +18,25 @@
     >
       計画、設計、事業計画認定申請(経済産業省)、接続検討申込(電力会社)、補助金申請(経済産業省、環境省)、施工、発電管理、パネル・PCS等の保守管理、除草、除雪等をワンストップサービスで行います。
     </h3>
-  </div>
+    <div class="mt-lg space-y-reg md:grid md:grid-cols-3">
+      <BaseAchieveCard
+        location="石川県かほく市"
+        name="松浜太陽光発電所"
+        watts="500"
+        :img="'images/pastwork/past-work-img-1.jpg'"
+      />
+      <BaseAchieveCard
+        location="富山県小矢部市"
+        name="水島第一太陽光発電所"
+        watts="300"
+        :img="'images/pastwork/past-work-img-2.jpg'"
+      />
+      <BaseAchieveCard
+        location="富山県小矢部市"
+        name="水島第二太陽光発電所"
+        watts="480"
+        :img="'images/pastwork/past-work-img-3.jpg'"
+      />
+    </div>
+  </section>
 </template>
---
+++ app/components/BaseAchieveCard.vue
@@ -0,0 +1,37 @@
+<script setup>
+//  Props
+const props = defineProps({
+  location: { type: String, required: true },
+  name: { type: String, required: true },
+  watts: { type: String, required: true },
+  img: { type: String, requird: true },
+});
+</script>
+
+<template>
+  <article
+    class="border-2 border-brand-blue w-[90%] mx-auto rounded-sm p-reg space-y-4 h-full"
+  >
+    <div class="space-y-1">
+      <p class="blue-bg-wrapper">所在地</p>
+      <h4 class="font-bold text-neutral-dark tracking-wider text-2xl">
+        {{ location }}
+      </h4>
+    </div>
+    <div class="space-y-1">
+      <p class="blue-bg-wrapper">発電所名</p>
+      <h4 class="font-bold text-neutral-dark tracking-wider text-2xl">
+        {{ name }}
+      </h4>
+    </div>
+    <div class="space-y-1">
+      <p class="blue-bg-wrapper">発電容量</p>
+      <h4 class="font-bold text-neutral-dark tracking-wider text-2xl">
+        {{ watts }}kW
+      </h4>
+    </div>
+    <figure class="rounded-sm">
+      <img :src="img" alt="solar panel display" class="rounded-sm" />
+    </figure>
+  </article>
+</template>
