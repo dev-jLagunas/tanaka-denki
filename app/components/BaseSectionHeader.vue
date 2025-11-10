e46c482c142b63fdcfbffd38939e28abfac508eb
Juan Lagunas <dev.jlagunas@gmail.com>
2025-11-10 14:45:14 +0900

    test commit

--- app/components/BaseSectionHeader.vue
+++ app/components/BaseSectionHeader.vue
@@ -3,11 +3,13 @@ const props = defineProps({
   title: { type: String, required: true },
   icon: { type: String, required: false }, // e.g. "mdi:home"
   align: { type: String, default: "center" },
+  sectionId: { type: String, required: false },
 });
 </script>
 
 <template>
   <header
+    :id="sectionId"
     class="flex-row-center gap-10 bg-brand-blue text-neutral-light my-10 py-5 md:w-1/2 lg:mt-28"
   >
     <h2 class="text-3xl font-medium tracking-widest text-balance">
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,10 @@
   <main>
     <HeroMobile class="md:hidden" />
     <HeroDesktop class="hidden md:block" />
-    <BaseSectionHeader title="会社案内" icon="mdi:office-building" />
+    <BaseSectionHeader
+      title="会社案内"
+      icon="mdi:office-building"
+      sectionId="work-info"
+    />
   </main>
 </template>
