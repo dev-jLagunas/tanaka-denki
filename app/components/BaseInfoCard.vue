f4c1be10eb8d03899dd5772518cdda8d41b10472
Juan Lagunas <dev.jlagunas@gmail.com>
2025-11-10 16:00:32 +0900

    feat: set up html, styles, and props for base info card

--- app/components/BaseInfoCard.vue
+++ app/components/BaseInfoCard.vue
@@ -1,5 +1,49 @@
+<script setup>
+const props = defineProps({
+  bgColor: {
+    type: String,
+    default: "bg-lightest-blue",
+  },
+  number: {
+    type: [String, Number],
+    required: true,
+  },
+  subheading: {
+    type: String,
+    required: true,
+  },
+  longCopy: {
+    type: String,
+    default: "", // optional text
+  },
+  imgSrc: {
+    type: String,
+    required: true,
+  },
+  imgAlt: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
 <template>
-  <div>
-    <p>base work info</p>
-  </div>
+  <article :class="`work-info-card ${bgColor}`">
+    <header class="space-y-sm px-sm grow">
+      <div class="work-info-card-heading">
+        <p class="font-bold text-brand-blue text-3xl">{{ number }}</p>
+      </div>
+      <h3 class="work-info-card-subheading">
+        {{ subheading }}
+      </h3>
+      <!-- optional long copy -->
+      <p v-if="longCopy" class="long-copy-text text-2xl my-sm">
+        {{ longCopy }}
+      </p>
+    </header>
+
+    <figure class="w-full h-[350px] mt-8 rounded-sm overflow-hidden shrink-0">
+      <img :src="imgSrc" :alt="imgAlt" class="h-full w-full object-cover" />
+    </figure>
+  </article>
 </template>
--- app/components/WorkInfo.vue
+++ app/components/WorkInfo.vue
@@ -6,6 +6,21 @@
     <h3 class="long-copy-text text-2xl mt-sm md:text-3xl md:leading-9">
       お客様の諸課題をすべて解決いたします
     </h3>
-    <BaseInfoCard />
+    <div class="md:grid md:grid-cols-2 md:gap-reg lg:grid-cols-3">
+      <BaseInfoCard
+        bgColor="bg-lightest-blue"
+        number="1"
+        subheading="年次・月次の点検・報告"
+        longCopy="長期にわたり、安定した電力を確保するため、年次・月次点検を行います。"
+        imgSrc="/images/hero/hero-slide-1.webp"
+      />
+      <BaseInfoCard
+        bgColor="bg-lightest-blue"
+        number="2"
+        subheading="パネル・パワーコンディショナー等の修復"
+        longCopy="焼損コネクタの撤去、新コネクタの取付け、配線接続により故障復旧を図ります。"
+        imgSrc="/images/hero/hero-slide-1.webp"
+      />
+    </div>
   </section>
 </template>
