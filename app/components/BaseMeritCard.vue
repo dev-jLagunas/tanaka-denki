1e7c03ed0eaa5448cf3bca9c941ebd3f9a115fa7
Juan Lagunas <dev.jlagunas@gmail.com>
2025-11-10 19:45:14 +0900

    feat: set up html and styling for base merit card

---
+++ app/components/BaseMeritCard.vue
@@ -0,0 +1,31 @@
+<script setup>
+//  Props
+const props = defineProps({
+  number: {
+    type: [String, Number],
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <article
+    class="border-2 border-brand-blue px-reg py-lg mx-reg rounded-sm mt-reg"
+  >
+    <div class="flex-row-between">
+      <p class="bg-brand-blue text-neutral-light w-fit px-6 py-1 rounded-sm">
+        ポイント {{ number }}
+      </p>
+      <Icon name="mdi:check" size="60" class="text-brand-green" />
+    </div>
+    <p
+      class="font-bold text-neutral-dark tracking-wider mt-reg text-2xl md:text-4xl lg:text-6xl"
+    >
+      {{ text }}
+    </p>
+  </article>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,18 @@
   <main>
     <HeroMobile class="md:hidden" />
     <HeroDesktop class="hidden md:block" />
-
     <WorkInfo />
+    <BaseSectionHeader
+      bgColor="bg-brand-blue"
+      title="特記事項"
+      icon="mdi:thumbs-up"
+      sectionId="work-info"
+    />
+    <h3 class="long-copy-text px-reg md:leading-9 text-2xl md:text-3xl">
+      お客様のメリット
+    </h3>
+    <BaseMeritCard number="1" text="一体的保守管理による経費節減" />
+    <BaseMeritCard number="2" text="異常の早期発見による損失の縮減" />
+    <BaseMeritCard number="3" text="メンテナンス不足による損失の縮減" />
   </main>
 </template>
