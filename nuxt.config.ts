2d803f5ff884e55592a27b436d7f31500e8baa48
Juan Lagunas <dev.jlagunas@gmail.com>
2025-11-13 11:32:35 +0900

    chore: fix netlify hosting issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   },
   ssr: false,
   app: {
-    baseURL: "/tanaka-denki/",
+    baseURL: "/",
     buildAssetsDir: "assets",
   },
   modules: ["@nuxt/icon", "@nuxtjs/google-fonts"],
