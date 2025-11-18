4e72620444e4763667ab6884c0334dd1201c1892
Juan Lagunas <dev.jlagunas@gmail.com>
2025-11-18 18:03:34 +0900

    chore: add image to blog

--- app/pages/blog/[id].vue
+++ app/pages/blog/[id].vue
@@ -23,15 +23,15 @@ const htmlBody = documentToHtmlString(fields.blogBody);
 
 // Extract image
 let bodyImage = null;
+let extraImg1 = null;
 
-if (fields.blogMainImage?.sys?.id) {
-  const imgId = fields.blogMainImage.sys.id;
-  const asset = data.includes.Asset.find((a) => a.sys.id === imgId);
-
-  if (asset) {
-    bodyImage = `https:${asset.fields.file.url}`;
-  }
+function getAssetUrl(id) {
+  const asset = data.includes.Asset.find((a) => a.sys.id === id);
+  return asset ? `https:${asset.fields.file.url}` : null;
 }
+
+bodyImage = getAssetUrl(fields.blogMainImage?.sys?.id);
+extraImg1 = getAssetUrl(fields.blogExtraImg1?.sys?.id);
 </script>
 
 <template>
@@ -43,14 +43,14 @@ if (fields.blogMainImage?.sys?.id) {
       sectionId="blog-contents"
     />
     <section
-      class="px-reg long-copy-text text-primary-dark space-y-sm pb-lg light-underline lg:grid lg:grid-cols-2 lg:items-center lg:gap-lg"
+      class="px-reg long-copy-text text-primary-dark space-y-sm pb-lg light-underline"
     >
       <img
         v-if="bodyImage"
         :src="bodyImage"
-        class="w-auto rounded-md shadow-md my-8 mx-auto max-h-[600px] md:object-contain"
+        class="w-auto rounded-md shadow-md my-8 mx-auto max-h-[500px] md:object-contain"
       />
-      <div>
+      <div class="w-[75%] mx-auto">
         <p class="text-sm text-slate-400 -mb-1">{{ fields.blogDate }}</p>
 
         <h1 class="mb-sm text-lg font-bold lg:text-xl">
@@ -58,6 +58,11 @@ if (fields.blogMainImage?.sys?.id) {
         </h1>
         <!-- Rich Text Content -->
         <div class="mb-reg prose max-w-none blog-body" v-html="htmlBody"></div>
+        <img
+          v-if="extraImg1"
+          :src="extraImg1"
+          class="max-h-[400px] rounded-md shadow-md col-span-2 mx-auto h-1/2 my-lg"
+        />
         <NuxtLink to="/blog" class="red-cta-btn">前のページに戻る</NuxtLink>
       </div>
     </section>
